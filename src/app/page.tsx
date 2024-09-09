dca38faea8acbef1cf252cbbb393bdb1cb58e51c
Mark Faga <mjfaga@gmail.com>
2024-09-09 13:18:56 -0400

    feat: add @stigg to break build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import {StiggProvider} from '@stigg/react-sdk';
+
 export default function Home() {
-  return <>This is some content</>;
+  return (
+    <>
+      This is some content
+      <StiggProvider apiKey="<STIGG_CLIENT_API_KEY>">more content</StiggProvider>
+    </>
+  );
 }
